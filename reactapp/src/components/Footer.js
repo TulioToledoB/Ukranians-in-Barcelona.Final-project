a74494adc0c720b9eb4c0f8ba75a36c8f1184984
carmela0501 <oropesacarmela0207@gmail.com>
2023-10-17 19:26:33 +0200

    adjust footer.js/footer.css

--- reactapp/src/components/Footer.js
+++ reactapp/src/components/Footer.js
@@ -6,37 +6,37 @@ function Footer() {
     <footer className="bg-dark text-white pt-1 pb-1">
       <div className="container text-center text-md-left">
         <div className="row text-center text-md-left">
-          <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mt-2 mb-1">
+          <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mt-1 mb-1">
             <h5 className="text-uppercase mb-2 font-weight-bold text-warning">ABOUT US</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere dui sem. Aliquam posuere a massa sit amet dignissim. Mauris nisl turpis, malesuada ac odio vel, pharetra vulputate nibh. Mauris quis mattis nisi. Mauris urna nulla, eleifend a eros id, elementum posuere ante. Vestibulum vel eros eget elit hendrerit varius id eu risus. Vestibulum vestibulum.</p>
           </div>
-          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-2 mb-1">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-1 mb-1">
             <h5 className="text-uppercase mb-2 font-weight-bold text-warning">RESOURCES</h5>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <a href="#" className="text-white" style={{ textDecoration: 'none' }}>Social Services</a>
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <a href="#" className="text-white" style={{ textDecoration: 'none' }}>Legalization in Spain</a>
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <a href="#" className="text-white" style={{ textDecoration: 'none' }}>Lawyers and Policy</a>
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <a href="#" className="text-white" style={{ textDecoration: 'none' }}>Job Order</a>
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <a href="#" className="text-white" style={{ textDecoration: 'none' }}>Others</a>
             </p>
           </div>
-          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-2 mb-1">
-            <h5 className="text-uppercase mb-2 font-weight-bold text-warning">CONTACTS</h5>
-            <p>
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-1 mb-1">
+            <h5 className="text-uppercase mb-1 font-weight-bold text-warning">CONTACTS</h5>
+            <p style={{ marginBottom: '5px' }}>
               <i className="fas fa-house mr-3"></i> Barcelona, Spain
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <i className="fas fa-envelope mr-3"></i> ukrainianInBcn@gmail.com
             </p>
-            <p>
+            <p style={{ marginBottom: '5px' }}>
               <i className="fas fa-phone mr-3"></i> +34 123456789
             </p>
           </div>
@@ -52,7 +52,7 @@ function Footer() {
             </p>
           </div>
           <div className="row d-flex justify-content-center">
-            <div className="text-center">
+            <div className="text-center" style={{ marginBottom: '5px' }}>
               <ul className="list-unstyled list-inline pt-1 pb-1 mb-1">
                 <li className="list-inline-item">
                   <a href="#" className="btn-floating btn-sm text-white" style={{ fontSize: '12px' }}>
@@ -70,7 +70,7 @@ function Footer() {
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="#" className="btn-floating btn-sm text-white" style={{ fontSize: '15px' }}>
+                  <a href="#" className="btn-floating btn-sm text-white" style={{ fontSize: '12px' }}>
                     <i className="fab fa-linkedin-in"></i>
                   </a>
                 </li>
