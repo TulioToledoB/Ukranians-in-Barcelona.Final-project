d71659c276aa9c1a0dc21b2b57a678057d89b032
ElenaPetrash <127974045+ElenaPetrash@users.noreply.github.com>
2023-11-09 15:11:22 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USERNAME,
   host: "localhost",
-  database: "ukrainians_in_barcelona",
+  database: "ukranians_in_barcelona",
   password: process.env.DB_PASSWORD,
   port: 5432,
 });
