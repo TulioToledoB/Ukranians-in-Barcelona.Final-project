d7522c8dd79848171dce3b203edfa18c1aa67d6c
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2023-11-17 23:41:51 +0100

    Css de CardSectorCss

--- reactapp/src/components/SectorsBody/Hospitals.js
+++ reactapp/src/components/SectorsBody/Hospitals.js
@@ -5,7 +5,6 @@ import "./Sectors.css";
 import BigCard from "./SearchFormsSectors/BigCard";
 import { useTranslation } from "react-i18next";
 import i18n from "../../i18next";
-import image from "/home/l0013/Desktop/MigraCode/FinalProject/Ukranians-in-Barcelona.Final-project/reactapp/src/components/SectorsBody/ddd .png"
 
 
 function Hospitals() {
@@ -36,7 +35,7 @@ function Hospitals() {
   return (
     <div className="hospitals_body">
       <div className="title_div">
-        <img src={image}></img>
+      
         <h2 className="title_sectors">{t("title_sector_hospitals")}</h2>
       </div>
       <div className="allBody"> 
